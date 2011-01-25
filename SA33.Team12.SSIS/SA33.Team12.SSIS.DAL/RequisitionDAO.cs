ae9258420800856708d4177b6b0319163d66b47c
aravinddgl <aravinddgl@gmail.com>
2011-01-25 08:53:36 +0000

    Updated some methods

--- SA33.Team12.SSIS/SA33.Team12.SSIS.DAL/RequisitionDAO.cs
+++ SA33.Team12.SSIS/SA33.Team12.SSIS.DAL/RequisitionDAO.cs
@@ -1,17 +1,17 @@
 ﻿/***
- * Author: Naing Myo Aung (A0076803A)
+ * Author: Sundar Aravind (A0076790U)
  * Initial Implementation: 23/Jan/2011
+ * Modified on: 25/Jan/2011
  ***/
 
 using System;
 using System.Web;
 using System.Collections.Generic;
 using System.ComponentModel;
-
+using System.Linq;
 using SA33.Team12.SSIS.DAL;
-using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
-using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
 using SA33.Team12.SSIS.DAL.DTO;
+using System.Transactions;
 
 namespace SA33.Team12.SSIS.DAL
 {
@@ -21,54 +21,137 @@ namespace SA33.Team12.SSIS.DAL
         {
             try
             {                
-               // context.AddToRequsitions(requisition);                
+               context.AddToRequisitions(requisition);
+               foreach (RequisitionItem reqItem in requisition.RequisitionItems)
+               {
+                   context.AddToRequisitionItems(reqItem);                   
+               }             
+             
+               foreach (SpecialRequisitionItem splItem in requisition.SpecialRequisitionItems)
+               {                              
+                   context.AddToSpeicalStationeries(splItem.SpeicalStationery);
+                   context.AddToSpecialRequisitionItems(splItem);                   
+               }
+
+               var status = (from s in context.Statuses where s.Name == "Pending" select s).FirstOrDefault<Status>();
+
+               UpdateRequisitionStatus(requisition, status);
+               context.SaveChanges();
             }
             catch (Exception ex)
             {
-                ExceptionManager exceptionManager = EnterpriseLibraryContainer.Current.GetInstance<ExceptionManager>();
-                exceptionManager.HandleException(ex, "Policy");
+                
             }
             
         }
 
-        public void UpdateRequisitionStatus(Requisition requisition)
-        {
-            throw new System.NotImplementedException();
+
+        public void UpdateRequisitionStatus(Requisition requisition, Status status)
+ {
+            try
+            {
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    var tempRequisition = (from p in context.Requisitions
+                                           where p.RequisitionID == requisition.RequisitionID
+                                           select p).FirstOrDefault<Requisition>();
+
+                    tempRequisition.Status = status;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
         }
 
         public void ApproveRequisition(Requisition requisition)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    var tempRequisition = (from p in context.Requisitions
+                                           where p.RequisitionID == requisition.RequisitionID
+                                           select p).FirstOrDefault<Requisition>();
+
+                    var status = (from s in context.Statuses where s.Name == "Approved & Pending" select s).FirstOrDefault<Status>();
+
+                    UpdateRequisitionStatus(tempRequisition, status);
+                    //tempRequisition.ApprovedBy = requisition.ApprovedBy;
+                    //tempRequisition.DateApproved = requisition.DateApproved;
+                    //context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public void ApproveRequisition(List<Requisition> requisitions)
         {
-            throw new System.NotImplementedException();
+            if (requisitions != null)
+            {
+                foreach (Requisition tempReq in requisitions)
+                {
+                    ApproveRequisition(tempReq);
+                }
+            }
         }
 
-        public void CancelRequisition()
+        public void CancelRequisition(Requisition cancelRequisition)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    var tempRequisition = (from p in context.Requisitions
+                                           where p.RequisitionID == cancelRequisition.RequisitionID
+                                           select p).FirstOrDefault<Requisition>();
+
+                    var status = (from s in context.Statuses where s.Name == "Cancelled" select s).FirstOrDefault<Status>();
+
+                    UpdateRequisitionStatus(tempRequisition, status);
+                    //tempRequisition.ApprovedBy = requisition.ApprovedBy;
+                    //tempRequisition.DateApproved = requisition.DateApproved;
+                    //context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
-        public void UpdateRequisition(Requisition requisition)
+        public void UpdateRequisition(Requisition updateRequisition)
+
         {
-            throw new System.NotImplementedException();
+            var tempReq = (from r in context.Requisitions
+                           where r.RequisitionID == updateRequisition.RequisitionID
+                           select r).FirstOrDefault<Requisition>();
+
+            tempReq = updateRequisition;
+            context.SaveChanges();
         }
 
-        public void GetRequisitionByCategory(Category category, RequisitioinSearchDTO requisitionSearchDTO)
+        public List<Requisition> GetRequisitionByCategory(Category category, RequisitioinSearchDTO requisitionSearchDTO)
         {
-            throw new System.NotImplementedException();
+            return null;
         }
 
-        public void GetRequisitionByDepartment(Department department, RequisitioinSearchDTO requisitionSearchDTO)
+        public List<Requisition> GetRequisitionByDepartment(Department department, RequisitioinSearchDTO requisitionSearchDTO)
         {
-            throw new System.NotImplementedException();
+            return null;
         }
 
-        public void GetRequisitionByEmployee(User user, RequisitioinSearchDTO requisitionSearchDTO)
+        public List<Requisition> GetRequisitionByEmployee(User user, RequisitioinSearchDTO requisitionSearchDTO)
         {
-            throw new System.NotImplementedException();
+            return null;
         }
     }
 }
