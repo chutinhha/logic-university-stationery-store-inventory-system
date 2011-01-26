76351d2ec2091bedeaebe8b4d8ba4c5c103fe33e
sa33t12 <sa33t12@gmail.com>
2011-01-26 06:03:45 +0000

    Ha ha

---
+++ SA33.Team12.SSIS/SA33.Team12.SSIS.DAL/AdjustmentVoucher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SA33.Team12.SSIS.DAL
+    //Created by Anthony 26 Jan 2011
+{
+    [MetadataType(typeof(AdjustmentVoucherMetaData))]
+    public partial class AdjustmentVoucher
+    {
+    }
+
+    public class AdjustmentVoucherMetaData
+    {
+        [Required(ErrorMessage="Please enter item code.")]
+        [RegularExpression(".{4}", ErrorMessage="Please enter valid item code.")]
+        public string StationeryID { get; set; }
+    }
+}
