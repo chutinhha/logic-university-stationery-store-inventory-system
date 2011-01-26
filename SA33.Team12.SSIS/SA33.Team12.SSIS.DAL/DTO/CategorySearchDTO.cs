255a31bbfcaeab7a65643aa6a41ba60a593612f4
victortong3@gmail.com <victortong3@gmail.com@4d150484-1778-5214-4088-bb3b5b55fb44>
2011-01-26 07:38:07 +0000

    added CategorySearchDTO

---
+++ SA33.Team12.SSIS/SA33.Team12.SSIS.DAL/DTO/CategorySearchDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SA33.Team12.SSIS.DAL.DTO
+{
+    class CategorySearchDTO
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public bool IsApproved { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+        public int ApprovedBy { get; set; }       
+    }
+}
